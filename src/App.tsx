6a1028c372e3f64a3ad5e2067e644fb5fd332b1d
root <root@DESKTOP-RJ4KCOB.localdomain>
2021-05-03 01:11:36 -0400

    Add Firebase SDK and initialize Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import './App.css';
 import React from 'react';
 
@@ -7,22 +8,37 @@ import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { pathToFileURL } from 'node:url';
 
-firebase.initializeApp({
-  // configure app info
-});
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PRJ_ID,
+  storageBucket: process.env.REACT_APP_STGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEAS_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 
+function App() { 
 
-function App() {
+  console.log("Hello World");
+  console.log(firebaseConfig);
+
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <header className="App-header"></header>
+      <div className="App-body">
+        <h1>Hello Chat App</h1>
+      </div>
     </div>
   );
 }
